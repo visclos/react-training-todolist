fce5c78ae3d160d685b61c1c28acfb879c7dbc28
Julia Hu <juliahukoshi@gmail.com>
2023-01-14 10:10:45 +0100

    feat: added all css

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,13 @@
-import React, { useState } from "react";
+import React from "react";
 import "./App.css";
 import { Task } from "./components/Task";
 import { TaskFormModal } from "./components/TaskFormModal";
 import { data } from "./data/tasks";
 
-// 1- Replace the hardocded app title with the const "title"
-// 2- Create a Header component in the components file and replace the
-// header div with the Header component.
-// 3- Pass the Header component the const "title" as prop
-
-// Create an interface Task in the model folder that will 
-// Later on you can transform this interface to a class with its own methods
-
-// App.tsx should not have the responsibility to iterate over the tasks.
-// Create an intermediate component that does that and display each task using the Task
-// component.
-
-// The task modal is rendered in the App.tsx.
-// Which means that initial values is empty when the component is rendered...
-// use a react hook in the modal to make the component listen to the changes of initial values
-
 const App = () => {
   const title = "To do list";
-  let taskToEdit: any = null;
+  const tasks = data;
+  const taskToEdit: any = null;
 
   const updateTaskState = (taskId: number) => {
     console.error("I need to be implemented");
